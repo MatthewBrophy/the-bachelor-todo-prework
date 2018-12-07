f9cce45b7c17b72aff1769aaabd912172c630be5
MatthewBrophy <Brophy3030@gmail.com>
2018-12-06 18:24:31 -0800

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |year, person|
+    if year == season
+      person.each do |attribute, _value|
+        if attribute['status'] == 'Winner'
+          return attribute['name'].partition(' ').first
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |_year, person|
+    person.each do |attribute, _value|
+      if attribute['occupation'] == occupation
+        return attribute['name']
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  contestants = []
+  data.each do |_year, person|
+    person.each do |attribute, _value|
+      if attribute['hometown'] == hometown
+        contestants << attribute['name']
+      end
+    end
+  end
+  contestants.count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |_year, person|
+    person.each do |attribute, _value|
+      if attribute['hometown'] == hometown
+        return attribute['occupation']
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  contestant_ages = []
+  data.each do |year, person|
+    if year == season
+      person.each do |attribute, _value|
+        contestant_ages << attribute['age'].to_f
+        
+      end
+    end
+  end
+  (contestant_ages.sum / contestant_ages.length).round
 end
